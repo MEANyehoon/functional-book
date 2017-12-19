a831aff4c212174c836ccbde850a5e07ca42230e
yinhang <yinhang@dehuinet.com>
2017-12-19 18:19:49 +0800

    allOf

--- src/utils.js
+++ src/utils.js
@@ -11,11 +11,26 @@ const doWhen = (condition, action) => {
     }
 }
 
+exports.allOf = function () {
+    return !!_.reduce(_.toArray(arguments), function(ret, arg) {
+        if (ret){
+            ret = (arg instanceof Function) ? arg() : arg;
+        } 
+        return ret;
+
+    }, true);
+}
+
+exports.rCurry = method => x => obj => method(obj, x);
+
+
+
 exports.isIndexed = x => _.isArray(x) || _.isString(x);
 
 exports.existy = existy;
 
 exports.truthy = truthy;
+
 
 exports.executeIfHasField = (target, name) => doWhen(existy(target[name]), () => {
     const result = _.result(target, name);
